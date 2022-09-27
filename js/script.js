dd32d9e8056241a81029a31756ff3520fc6515ae
nikhilwebdeveloper <108711205+nikhilwebdeveloper@users.noreply.github.com>
2022-09-27 04:26:47 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ function openmore(){
     document.getElementById("more-menu").style.transform = 'scaleY(1)'
     document.getElementById("open-more-li").style.display = 'none'
     document.getElementById("close-more-li").style.display = 'inline-block'
+    document.getElementById("close-more").style.display = "inline-block"
     // document.getElementById("close-more").style.zIndex = '2'
     // document.getElementById("open-more").style.zIndex = '1'
     document.getElementById("more-after").style.transform = 'rotate(90deg)'
@@ -19,3 +20,28 @@ function closemore(){
     // document.getElementById("open-more").style.zIndex = '2'
     document.getElementById("more-after").style.transform = 'rotate(180deg)'
 }
+
+// MOBILE MENU
+function mobilemenu(){
+    document.getElementById("navbar").style.left = "0px"
+    document.getElementById("mobile-menu").style.display = "none"
+}
+function closemenu(){
+    document.getElementById("navbar").style.left = "-528px"
+    document.getElementById("mobile-menu").style.display = "inline-block"
+}
+
+// SCROLL HIDDEN
+
+window.addEventListener('scroll', function(){
+    let scrollhidden = document.getElementById('navbar');
+
+    if(window.innerWidth <= 500){
+        if(window.pageYOffset >= 1){
+            scrollhidden.style.left = '-528px'
+            document.getElementById('mobile-menu').style.display = 'inline-block'
+        }
+    }
+        
+
+});
